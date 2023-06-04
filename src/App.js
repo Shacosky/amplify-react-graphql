2a4c129059366c6e3667c26bff1466d35c0453a1
dperez <daniel.perezu@alfametrics.ai>
2023-06-03 23:17:56 -0400

    confgi json

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenido a mi portafolio</h1>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Soy [Tu nombre], un [tu profesión o descripción].
-        </p>
-        <p>
-          Aquí encontrarás información sobre mis proyectos y habilidades.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="#proyectos"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Ver mis proyectos
-        </a>
-        <a
-          className="App-link"
-          href="#contacto"
-        >
-          Contáctame
+          Learn React
         </a>
       </header>
-      <section id="proyectos" className="App-section">
-        <h2>Mis Proyectos</h2>
-        {/* Aquí puedes agregar una lista de tus proyectos */}
-      </section>
-      <section id="contacto" className="App-section">
-        <h2>Contacto</h2>
-        {/* Aquí puedes agregar un formulario de contacto o información de contacto */}
-      </section>
-      <footer className="App-footer">
-        &copy; {new Date().getFullYear()} [Tu nombre o nombre de la empresa]
-      </footer>
     </div>
   );
 }
