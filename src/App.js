c745732763ef15e644369357619dca86cf8b1b7d
dperez <daniel.perezu@alfametrics.ai>
2023-06-03 21:35:44 -0400

    cambiando a portafolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bienvenido a mi portafolio</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Soy [Tu nombre], un [tu profesión o descripción].
+        </p>
+        <p>
+          Aquí encontrarás información sobre mis proyectos y habilidades.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#proyectos"
         >
-          Learn React
+          Ver mis proyectos
+        </a>
+        <a
+          className="App-link"
+          href="#contacto"
+        >
+          Contáctame
         </a>
       </header>
+      <section id="proyectos" className="App-section">
+        <h2>Mis Proyectos</h2>
+        {/* Aquí puedes agregar una lista de tus proyectos */}
+      </section>
+      <section id="contacto" className="App-section">
+        <h2>Contacto</h2>
+        {/* Aquí puedes agregar un formulario de contacto o información de contacto */}
+      </section>
+      <footer className="App-footer">
+        &copy; {new Date().getFullYear()} [Tu nombre o nombre de la empresa]
+      </footer>
     </div>
   );
 }
